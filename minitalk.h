ba4a6731ed7448810e52f3e724da5080582da7d5
mgering <mgering@mail.de>
2024-04-18 14:15:47 +0200

    norminette fix

--- minitalk.h
+++ minitalk.h
@@ -6,10 +6,9 @@
 /*   By: mgering <mgering@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:15:48 by mgering           #+#    #+#             */
-/*   Updated: 2024/04/17 15:23:52 by mgering          ###   ########.fr       */
+/*   Updated: 2024/04/18 14:14:56 by mgering          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef MINITALK_H
 # define MINITALK_H
@@ -21,6 +20,5 @@
 # include <signal.h>
 # include "resource/ft_printf/ft_printf.h"
 # include "resource/libft/libft.h"
-
 
 #endif
